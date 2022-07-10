bb8f77b0fe6408b7c8c73df3d94d487b70ed38a9
Daniel Ortiz <75407003+DanielGGO@users.noreply.github.com>
2022-07-10 00:21:37 -0500

    Add files via upload
    En este pequeño programa el titulo no es muy descriptible, de lo que trata es que al momento de dar tu año de nacimiento, el programa te lanzara tu edad en (años, dias, meses, horas, minutos y segundos).. :)

---
+++ C++/Edad en años,meses,minutosysegundos.cpp
@@ -0,0 +1,29 @@
+//introducimos nuestra libreria
+#include "iostream"
+using namespace std;
+int tope = 2022;
+int mes = 12, dia = 365, hora = 24, minuto = 60, segundo = 60;
+//introducimos nuestra funcion principal
+int main(){
+	int edad,years,meses,dias,horas,minutos,segundos; //agregamos nuestras variables
+	cout<<"ingresa tu fecha de nacimiento en 4 digitos: "<<endl;
+	cin>>edad;
+	
+	years=tope-edad;
+	
+	cout<<"tu edad es: "<<years<<endl;
+	meses=years*mes;
+	cout<<"tu edad en meses es: "<<meses<<endl;
+	dias=years*mes;
+	dias=years*dia;
+	cout<<"tu edad en dias es: "<<dias<<endl;
+	horas=dias*hora;
+	cout<<"tu edad en horas es: "<<horas<<endl;
+	minutos=horas*hora;
+	cout<<"tu edad en minutos es: "<<minutos<<endl;
+	segundos = minutos*segundos;
+	cout<<"tu edad en segundos es: "<<segundos<<endl;
+	
+	return 0;
+cin.get(); //se utiliza para indicar que el programa se ha ejecutado de manera exitosa
+}
