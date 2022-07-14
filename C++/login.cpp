52c60f5870d7c7d9a0215cb9e6a8a5bde9faa853
Daniel Ortiz <75407003+DanielGGO@users.noreply.github.com>
2022-07-14 12:45:32 -0500

    Add files via upload

--- C++/login.cpp
+++ C++/login.cpp
@@ -1,84 +1,103 @@
-//login en c++
-
- ***********************************************************
- *Error dentro de la comprobaci√≥n de usuario		   *
- *no verifica si es o no el verdadero usuario y contrae√±a  *
- ***********************************************************
-
-
-
-//introducimos librerias
 #include <iostream>
-#include <conio.h>
-#include <windows.h>
 #include <string>
-//Definimos las variables
-#define User "danielo"
-#define Pass "1234"
-#define Enter 13
-#define Backspace 8
-using namespace std; //colocamos el using para utilizar los datos que estan dentro de las librerias
+#include <cstdlib>
+#include <conio.h> // getch
+#include <vector>
+#define ENTER 13
+#define BACKSPACE 8
+#define INTENTOS 3
 
-int main(){
-	string usuario,password;
-	int contador=0;
-	bool ingresa = false;
-	do{
-		system("cls");
-		cout<<"***************************************************************"<<endl;
-		cout<<"***************************************************************"<<endl;
-		cout<<"\t\t\t                    Logueate"<<endl;
-		cout<<"\t\t\t          _____________________________"<<endl;
-		cout<<"\t\t                      Usuario: "<<endl;
-		getline(cin,usuario);
-		cout<<"\t\t\t          _____________________________"<<endl;
-		cout<<"\t\t                     Password: "<<endl;
-		getline(cin,password);
-		char caracter;
-		caracter=getch();
-		password="";
-		while(caracter!=Enter){
-			if(caracter!=Backspace){
-			password.push_back(caracter);
-			cout<<"*";
-			}else{
-				if(password.length()<0){
-					cout<<"\b\b";
-					password=password.substr(0,password.length()-1);
-				}
-			}caracter=getch();
-			
-		}
-		if( usuario==User && password==Pass ){
-		ingresa = true;
-		}else{
-			cout<<"\n\n\n el usuario y/o contrase√±a son incorrectos"<<endl;
-			cin.get();
-			contador++;
-		}
-		
-	}while(ingresa==false&&contador<3);
-	if(ingresa==false){
-		cout<<"\n\t usted no pudo ingresar al sistemas"<<endl;
-	}else{
-		cout<<"Bienvenido al sistemas"<<endl;
-	}
-	cin.get();
-	return 0;
+using namespace std;
+
+int main()
+{
+    vector<string> usuarios;
+    vector<string> claves;
+    usuarios.push_back("Danielo");
+    usuarios.push_back("Fernando");
+    usuarios.push_back("Gabriela");
+    usuarios.push_back("Kendy");
+    usuarios.push_back("Brenda");
+    
+    //ContraseÒas
+    claves.push_back("1234");
+    claves.push_back("456");
+    claves.push_back("789");
+    claves.push_back("abc");
+    claves.push_back("efg");
+
+    string usuario, password;
+
+    int contador = 0;
+    bool ingresa = false;
+
+    do
+    {
+    	
+        system("cls");
+        cout << "\t\t\tLOGIN DE USUARIO" << endl;
+        cout << "\t\t\t----------------" << endl;
+        cout << "\n\tUsuario: ";
+        getline(cin, usuario);
+
+        char caracter;
+
+        cout << "\tPassword: ";
+        caracter = getch();
+
+        password = "";
+
+        while (caracter != ENTER)
+        {
+
+            if (caracter != BACKSPACE)
+            {
+                password.push_back(caracter);
+                cout << "*";
+            }
+            else
+            {
+                if (password.length() > 0)
+                {
+                    cout << "\b \b";
+                    password = password.substr(0, password.length() - 1);
+                }
+            }
+
+            caracter = getch();
+        }
+
+        for (int i = 0; i < usuarios.size(); i++)
+        {
+            if (usuarios[i] == usuario && claves[i] == password)
+            {
+                ingresa = true;
+                break;
+            }
+        }
+
+        if (!ingresa)
+        {
+            cout << "\n\n\tEl usuario y/o password son incorrectos" << endl;
+            cin.get();
+            contador++;
+        }
+
+    } while (ingresa == false && contador < INTENTOS);
+
+    if (ingresa == false)
+    {
+        cout << "\n\tUsted no pudo ingresar al sistema. ADIOS" << endl;
+    }
+    else
+    {
+        cout << "\n\n\tBienvenido al sistema" << endl;
+        /* 
+        AquÌ va el cÛdigo del programa cuando el usuario ingresa sus credenciales correctas
+        */
+    }
+
+    cin.get();
+
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
